e0228c78c6cf306db72ff886296aea2f51eaa7b4
bandaBW <140257596+bandaBW@users.noreply.github.com>
2024-07-10 12:43:01 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+    return (
+    <View style={styles.container}>
+         <View className="flex-1 items-center justify-center bg-white">
+             <Text>Open up App.js to start working on your app!</Text>
+            
+        </View>
+    </View>
+    );
+  }
